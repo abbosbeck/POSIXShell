45927d865ddc1822dcef89378f6db965bf697558
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-03-13 15:47:04 +0500

    CODE-RUB: Add Every Type As a Class

---
+++ POSIXShell/Factories/Commands/ExitCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace POSIXShell.Factories.Commands
+{
+    public class ExitCommand : IRun
+    {
+        public ExitCommand()
+        {
+            Run("");
+        }
+        public void Run(string command)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
