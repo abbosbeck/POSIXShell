f0a338c67b7958a5a4ee319eecff69bbeda70725
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-03-13 15:47:28 +0500

    CODE-RUB: Use Factory Design Pattern

---
+++ POSIXShell/Factories/CommandFactory.cs
@@ -0,0 +1,22 @@
+﻿using POSIXShell.Factories.Commands;
+
+namespace POSIXShell.Factories
+{
+    public class CommandFactory
+    {
+        public static IRun GetCommand(string commandName)
+        {
+            ReadOnlySpan<char> span = commandName.AsSpan();
+            int spaceIndex = span.IndexOf(' ');
+            string commandType = spaceIndex == -1 ? commandName : new string(span.Slice(0, spaceIndex));
+
+            return commandType switch
+            {
+                "type" => new TypeCommand(commandName),
+                "exit" => new ExitCommand(),
+                "echo" =>new EchoCommand(commandName),
+                _ => new NoCommand(commandName),
+            };
+        }
+    }
+}
