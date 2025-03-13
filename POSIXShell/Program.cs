aa2185673b5e84f4cf5d856d49f8e7046c4c0753
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-03-13 15:47:49 +0500

    CODE-RUB: Call The Factory Class

--- POSIXShell/Program.cs
+++ POSIXShell/Program.cs
@@ -1,31 +1,11 @@
-﻿while (true)
+﻿using POSIXShell.Factories;
+using POSIXShell.Factories.Commands;
+
+while (true)
 {
     Console.Write("$ ");
     var command = Console.ReadLine();
 
-    if(command == "exit")
-    {
-        Environment.Exit(0);
-    }else if (command.StartsWith("echo"))
-    {
-        command = command.Substring(5);
-        Console.WriteLine(command);
-    }else if (command.StartsWith("type"))
-    {
-        command = command.Substring(5);
-        if (command == "exit" || command == "echo" || command == "type")
-        {
-            Console.WriteLine($"{command} is a shell builtin");
-        }
-        else
-        {
-            Console.WriteLine($"{command}: not found");
-        }
-    }
-    else
-    {
-        Console.WriteLine($"{command}: command not found");
-    }
-
+    CommandFactory.GetCommand(command);
 }
 
