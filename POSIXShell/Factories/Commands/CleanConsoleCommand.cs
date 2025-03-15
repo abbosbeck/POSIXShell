581b173011c08c0bfc2b18996067ed09f63db923
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-03-15 17:32:35 +0500

    Init Cls Command

---
+++ POSIXShell/Factories/Commands/CleanConsoleCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace POSIXShell.Factories.Commands
+{
+    public class CleanConsoleCommand : IRun
+    {
+        public CleanConsoleCommand(string commandName)
+        {
+            Run(commandName);
+        }
+        public void Run(string command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
