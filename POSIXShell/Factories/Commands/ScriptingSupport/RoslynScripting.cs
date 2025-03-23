e26a681afc6843d8ae8ff0c7fc5d33de75f4c6a3
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-03-23 17:20:49 +0500

    Create Roslyn Complier

---
+++ POSIXShell/Factories/Commands/ScriptingSupport/RoslynScripting.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+namespace POSIXShell.Factories.Commands.ScriptingSupport
+{
+    public class RoslynScripting
+    {
+        public static async Task<object> RunScript(string code)
+        {
+            return await CSharpScript.EvaluateAsync(code, ScriptOptions.Default);
+        }
+    }
+}
